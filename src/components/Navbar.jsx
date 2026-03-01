2be2f2d260e0ef5535da87379f3abdfff8f94d26
askadiliya <askadiliyan@gmail.com>
2026-03-01 23:09:21 +0700

    feat: add component navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { AiOutlineShoppingCart } from "react-icons/ai"; 
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>logo</div>
+        <div>
+            <div>
+                <AiOutlineShoppingCart />
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
